9b272ea3ce60a44aa45f206bbc1b59e5efc58c2b
liusos <esfera360@gmail.com>
2016-08-26 14:10:03 -0500

    mejorando el hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hola, empezaste bien!"
-
+    render html: "hola, empezaste bien! Continua!"
   end
 
 end
