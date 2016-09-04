e4c1b2a7692ae80df96a6ff44dabaa2f2f76d2eb
liusos <esfera360@gmail.com>
2016-09-04 12:42:02 -0500

    testeo de títulos

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @base_title = "COOPER"
-  end
+  
 
   test "should get root" do
     get root_url
@@ -13,19 +11,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "COOPER"
   end
 
   test "should get instrucciones" do
     get static_pages_instrucciones_url
     assert_response :success
-    assert_select "title", "Instrucciones | #{@base_title}"
+    assert_select "title", "Instrucciones | COOPER"
   end
 
   test "should get resultados" do
     get static_pages_resultados_url
     assert_response :success
-    assert_select "title", "Resultados | #{@base_title}"
+    assert_select "title", "Resultados | COOPER"
   end
 
 end
