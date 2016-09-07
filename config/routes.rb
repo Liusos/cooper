c63d21b22cb1cfbb878d01ebf50e8ae0fd0bbcf8
liusos <esfera360@gmail.com>
2016-09-07 15:52:14 -0500

    registro de depto y condo por Administrador, terminado

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+
 
   root 'static_pages#home'
 
-  get '/home',          to: 'static_pages#home'
-  get '/instrucciones', to: 'static_pages#instrucciones'
-  get '/resultados',    to: 'static_pages#resultados'
-  get '/contacto',      to: 'static_pages#contacto'
-  get '/signup',        to: 'users#new'
+  get   '/home',            to: 'static_pages#home'
+  get   '/instrucciones',   to: 'static_pages#instrucciones'
+  get   '/resultados',      to: 'static_pages#resultados'
+  get   '/contacto',        to: 'static_pages#contacto'
+  get   '/signup',          to: 'users#new'
+  post  '/signup',          to: 'users#create'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
