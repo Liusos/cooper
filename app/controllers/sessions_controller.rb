213625551ebc5d6016a07abc9c93dde41e778a22
liusos <esfera360@gmail.com>
2017-07-25 19:15:43 -0500

    ajustes necesarios pero aún sin resolver Place_issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,21 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-    user = User.find_by(depto: params[:session][:depto].downcase)
+    user = User.find_by(depto: params[:session][:depto].downcase) #parece que hay que actualizar los atributos
     if user && user.authenticate(params[:session][:password])
       log_in user
       if user.admin == true #this conditional avoid evaluate admin by Stat
-        redirect_to user
+        puts "««««««««»»»»»»»»"
+        puts user
+        puts "««««««««»»»»»»»»"
+        redirect_to user #aquí se detecta un problema cuando falta 'resources :user'
       else
         if current_user.stat.nil?
           redirect_to stats_path #ruta personalizada
-        elsif current_user.stat.svo_6.nil? #evalua si se contesto hasta el último atributo
+        elsif current_user.stat.svo_6.nil? #evalúa si se contesto hasta el último atributo
           redirect_to edit_stat_stat_step_path(current_user.stat.id, "basic_info")
         else
           redirect_back_or user
