bc2acf8baf505da020831c72d28174a329b32be1
liusos <esfera360@gmail.com>
2017-08-10 19:52:17 -0500

    new clean checkpoint for associations C-O

--- db/migrate/20170714224842_create_commons_outcomes.rb
+++
@@ -1,8 +0,0 @@
-class CreateCommonsOutcomes < ActiveRecord::Migration[5.0]
-  def change
-    create_table :commons_outcomes do |t|
-      t.references :common, foreign_key: true
-      t.references :outcome, foreign_key: true
-    end
-  end
-end
