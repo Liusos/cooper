064f4adedab76cfda88b9f36237093ab3b16d32c
liusos <esfera360@gmail.com>
2017-09-11 14:40:03 -0500

    little fix

--- app/models/common.rb
+++ app/models/common.rb
@@ -2,6 +2,6 @@ class Common < ApplicationRecord
   has_many :shares
   has_many :users, through: :shares
   belongs_to :place
-  has_many :transaction
+  has_many :transactions
   has_many :outcomes, through: :transactions
 end
